cf235128bb0f334f39d6f391ec8cfcd2146d0839
JACKLINEKARIUKI <jacklinewk71@gmail.com>
2022-08-21 10:07:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+
+function findMatching(array, aString){
+  const newArray = array.filter(element => element.toLowerCase() === aString.toLowerCase());
+  return newArray;
+}
+
+function fuzzyMatch(array, char){
+  const newArray = array.filter(element => element.indexOf(char) === char.indexOf(char));
+  return newArray;
+}
+
+function matchName(array, aString){
+    const newArray = array.filter(element => element.name === aString);
+    return newArray;
+}
